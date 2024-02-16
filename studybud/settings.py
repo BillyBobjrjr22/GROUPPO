7f52441bb7e5f256b0f6ac826662f98c562b65e9
Nathan Nemetz <ntnemetz@gmail.com>
2024-02-16 18:21:46 -0500

    general command

--- studybud/settings.py
+++ studybud/settings.py
@@ -18,6 +18,8 @@ import os
 import sys
 import dj_database_url
 
+
+DISABLE_COLLECTSTATIC=1
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
